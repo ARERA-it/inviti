1d9c49fa796eb97b9b680f7225d15cdcc48ddead
Iwan Buetti <iwan.buetti@gmail.com>
2019-01-17 23:40:18 +0100

    added devise and CAS authentication

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :registerable, :recoverable, :rememberable, :validatable
+  devise :cas_authenticatable, :trackable
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'pages/welcome', as: 'welcome'
   get 'pages/dashboard', as: 'dashboard'
+
+  root 'pages#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
