35476e9bfd0475478614f1c771bd59153bbd0faf
Iwan Buetti <iwan.buetti@gmail.com>
2019-03-26 17:33:41 +0100

    add logs

--- app/jobs/check_new_emails_job.rb
+++ app/jobs/check_new_emails_job.rb
@@ -2,6 +2,7 @@ class CheckNewEmailsJob < ApplicationJob
   queue_as :default
 
   def perform
+    Rails.logger.info("-> CheckNewEmailsJob.perform here.")
     Rake::Task['inviti:check_emails'].invoke
   end
 end
