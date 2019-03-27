0d9e80e09f7f4b2c8be9463fe3ff2ecc14598260
Iwan Buetti <iwan.buetti@gmail.com>
2019-03-27 14:15:39 +0100

    debug email check

--- app/jobs/check_new_emails_job.rb
+++ app/jobs/check_new_emails_job.rb
@@ -27,7 +27,7 @@ class CheckNewEmailsJob < ApplicationJob
         inv.email_from_address = "#{ff.mailbox}@#{ff.host}"
         inv.email_subject = e.subject
         inv.email_body = email_body.gsub("\r\n", "\n").gsub("\n\n", "\n").gsub(/<!-- (.)+(\n)?(.)+ -->/, "")
-        inv.email_body_preview = email_body_preview.gsub("\r\n", "\n").gsub("\n\n", "\n").gsub(/<!-- (.)+(\n)?(.)+ -->/m, "")
+        inv.email_body_preview = email_body_preview.gsub("\r\n", "\n").gsub("\n\n", "\n").gsub(/<!-- (.)+(\n)?(.)+ -->/, "")
         inv.email_received_date_time = datetime
         inv.save
 
