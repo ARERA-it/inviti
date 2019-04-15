718d96a81937fe1db96412c94fb0e644c4f8c53b
Iwan Buetti <iwan.buetti@gmail.com>
2019-04-15 11:28:05 +0200

    Add url and organizer on description

--- app/models/invitation_calendar.rb
+++ app/models/invitation_calendar.rb
@@ -2,20 +2,25 @@ class InvitationCalendar
 
   def self.generate_ics(inv, url)
     cal = Icalendar::Calendar.new
-    cal.event do |e|
+    description = []
+    description << inv.notes << "\n" if !inv.notes.blank?
+    description << "Organizzazione: #{inv.organizer}" if !inv.organizer.blank?
+    description << "URL invito: <a href='#{url}'>#{url}</a>"
 
+    cal.event do |e|
       e.dtstart     = inv.from_date_and_time
       if inv.to_date_and_time
-        e.dtend       = inv.to_date_and_time
+        e.dtend     = inv.to_date_and_time
       else
-        e.dtend       = inv.from_date_and_time + 1.hour
+        e.dtend     = inv.from_date_and_time + 1.hour
       end
       e.summary     = inv.title
-      e.description = inv.notes
+      e.description = description.join("\n")
       e.ip_class    = "PRIVATE"
-      e.organizer   = inv.organizer
+      # e.organizer   = inv.organizer # deve essere un ind. email
       e.location    = inv.location
       e.url         = url
+      # e.comment     = "this is a comment\n2nd line"
     end
     return [cal.to_ical, "invito_#{inv.id}.ics"]
   end
