7f81a5d49de0a4b7a9870ede89824106968ffcb1
Iwan Buetti <iwan.buetti@gmail.com>
2019-08-07 23:17:26 +0200

    Modifica messaggio email per incarico per evitare risposta tramite posta

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -24,16 +24,16 @@ require 'rails_helper'
 # `rails-controller-testing` gem.
 
 RSpec.describe GroupsController, type: :controller do
-
+  login_admin
   # This should return the minimal set of attributes required to create a valid
   # Group. As you add validations to Group, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    attributes_for(:group)
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    attributes_for(:group, name: "")
   }
 
   # This should return the minimal set of values that should be in the session
