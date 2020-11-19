4013671fbc70d6051046138840ee4d76ff2c4b9f
Iwan Buetti <iwan.buetti@gmail.com>
2020-11-19 12:00:37 +0100

    Revert "prova"
    This reverts commit 950d53b2aa5dca9dc22ea3c60d348a671302c88a.

--- update.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-git pull
-docker-compose -f docker-compose.production.yml build
-docker-compose -f docker-compose.production.yml down
-docker-compose -f docker-compose.production.yml up -d
